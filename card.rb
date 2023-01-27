db5a99efa3f39aa4cdd5a6ab2bb48997789e7539
Rishi <48538864+DevRishii@users.noreply.github.com>
2023-01-26 21:25:44 -0500

    removed the cards class, made card, deck,main

---
+++ card.rb
@@ -0,0 +1,17 @@
+class Card
+
+    def initialize(number, shape, color, shading)
+        @number = number
+        @shape = shape
+        @color = color
+        @shading = shading
+    end
+
+    def to_s
+        "[#{@number}, #{@shape}, #{@color}, #{@shading}]"
+    end
+        
+
+
+
+end
---
+++ main.rb
@@ -0,0 +1,7 @@
+require "./card.rb"
+require "./deck.rb"
+
+deck = Deck.new
+
+puts deck.length
+puts deck.shuffle
