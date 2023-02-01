94faf07d11b93d55964b753a1d4a1776ff6593ac
Mia <122577665+Mia0729@users.noreply.github.com>
2023-02-01 13:58:12 -0500

    I' done w/ getChoice, not sure. Can someone check?

--- player.rb
+++ player.rb
@@ -16,7 +16,30 @@ class Player
     # calls verifyCards with the 3 chosen cards 
     # returns true of chosen cards are a set; returns false if they skip or cards are not a set
     def getChoice(tableDeck)
-        
+        checkVal = false
+        sizeDeck = tableDeck.length()
+        while !checkVal
+            puts "Enter the card number you would like to choose."
+            puts "Card number starts from Zero(0) to #{sizeDeck} and split numbers with space."
+            choice = gets.chomp
+            chosenCard = Array.new
+            chosenCard = choice.split
+            chosenCard.each do |i|
+                if i.is_an_int
+                    if !(i >= 0) || !(i < sizeDeck)
+                        puts "Typed wrong Number"
+                        break
+                    end
+                    checkVal = true
+                else
+                    puts "Typed wrong Number"
+                    break
+                end
+            end
+            
+        end
+        return verifyCards(chosenCard[0], chosenCard[1], chosenCard[2])
+
     end
 
     #returns name
