3ec5d627626ec8bbafa2d0ce8751915d4d00dfd8
Hunter Ashburn <85366454+HunterAshburn@users.noreply.github.com>
2023-02-01 19:07:09 -0500

    featureIndex in Card class

--- card.rb
+++ card.rb
@@ -9,9 +9,21 @@ class Card
         @shading = shading
     end
 
-    #gets specified element of array by index
-    def index(x)
-        @deck[x]
+    #returns specified card feature by index
+    def featureIndex(x)
+        
+        case x
+
+        when x == 0
+            return @number
+        when x == 1
+            return @shape
+        when x == 2
+            return @color
+        when x == 3
+            return @shading
+        end
+
     end
 
     def to_s
